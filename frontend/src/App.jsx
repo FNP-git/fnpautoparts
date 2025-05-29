742b7f87dc6d418317c8ee75761f9d68b4cf5fb9
Amit Raj <amitraj4381@gmail.com>
2025-05-30 01:42:53 +0530

    updated *

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -30,7 +30,7 @@ const App = () => {
         <Route path="/warranty" element={<Warranty />} />
         <Route path="/return" element={<Return />} />
         <Route path="/privacy" element={<Privacy />} />
-        <Route path="*" element={<Navigate to="/" />} />
+        <Route path="*" element={<Home />} />
 
       </Routes>
     </>
